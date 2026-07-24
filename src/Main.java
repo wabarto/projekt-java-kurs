07a651dc67eda3d369eb6a57478ee7633926b759
wabarto <brtszwasilewski@gmail.com>
2026-07-24 15:59:37 +0200

    projekt-3: create feature 3

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,14 @@ public class Main {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
             // Press Ctrl+D to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("create calculator and create feature" + i);
+            System.out.println("create calculator and create feature and feature 3" + i);
         }
     }
 }
