6fd84b89b945874fde21396032d867ba11278b87
wabarto <brtszwasilewski@gmail.com>
2026-07-24 16:05:28 +0200

    projekt-5: create feature 5

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome1!");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
